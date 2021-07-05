243ed79b9df8da01f4e43dabcbfaa12c50a50497
jidanji <42198953+jidanji@users.noreply.github.com>
2021-07-05 23:53:07 +0800

    马良第一次提交

---
+++ Scripts/element-ui/trans.js
@@ -0,0 +1,36 @@
+﻿function buildNode(item, items, item2item) {
+    var ID = item.ID;
+    var item1 = {};
+    var firsrArray = $.grep(items, function (el, index) {
+        return el.PID == ID;
+    });
+    if (firsrArray.length) {
+        item1.children = [];
+    }
+    
+    for(sth in item)
+	{
+	item1[sth]=item[sth];
+	}
+    
+    item2item(item, item1);
+
+    $.each(firsrArray, function (index, el) {
+        item1.children.push(buildNode(el, items, item2item));
+    })
+    return item1;
+}
+
+function dataTrans(sth, item2item) {
+    var res = [];
+
+    var firsrArray = $.grep(sth, function (el, index) {
+        return el.PID == "";
+    });
+
+    $.each(firsrArray, function (index, el) {
+        res.push(buildNode(el, sth, item2item));
+    })
+
+    return res;
+}
